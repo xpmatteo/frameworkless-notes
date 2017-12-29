ec588bf9024f5842515f85aca03a31a841b6fac3
Matteo Vaccari <vaccari@pobox.com>
2017-12-29 23:38:47 +0100

    changing the title now works in index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 
 
+var notesApp = new NotesApp(new NotesUiAdapter(document))
 
+document.getElementById('note-editor-input').addEventListener('input', (event) => {
+  notesApp.onInput(event.target.value)
+})
